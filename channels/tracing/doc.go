d2ebd137848b3635a7d932350eb0a3da7faf6a8e
hantdev <captainnemot1k60@gmail.com>
2025-03-21 18:23:55 +0700

    Add tracing instrumentation and PostgreSQL setup for channels service

---
+++ channels/tracing/doc.go
@@ -0,0 +1,7 @@
+// Package tracing provides tracing instrumentation for Mitras channels service.
+//
+// This package provides tracing middleware for Mitras channels service.
+// It can be used to trace incoming requests and add tracing capabilities to
+// Mitras channels service.
+//
+package tracing
