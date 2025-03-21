ae69279a33376390a7a9490c5557fb35b105e65b
hantdev <captainnemot1k60@gmail.com>
2025-03-21 19:14:57 +0700

    Add private package for internal service communication and implement client management service

---
+++ clients/tracing/doc.go
@@ -0,0 +1,7 @@
+// Package tracing provides tracing instrumentation for Mitras clients service.
+//
+// This package provides tracing middleware for Mitras clients service.
+// It can be used to trace incoming requests and add tracing capabilities to
+// Mitras clients service.
+//
+package tracing
