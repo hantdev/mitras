472c72d64db3571cf68d7872869cd9d4f2f339cf
hantdev <captainnemot1k60@gmail.com>
2025-03-30 18:35:41 +0700

    Add WebSocket API implementation with handshake and request handling

---
+++ ws/api/transport.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/hantdev/mitras"
+	"github.com/hantdev/mitras/ws"
+	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/websocket"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	service             = "ws"
+	readwriteBufferSize = 1024
+)
+
+var (
+	errUnauthorizedAccess = errors.New("missing or invalid credentials provided")
+	errMalformedSubtopic  = errors.New("malformed subtopic")
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  readwriteBufferSize,
+		WriteBufferSize: readwriteBufferSize,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
+	logger *slog.Logger
+)
+
+// MakeHandler returns http handler with handshake endpoint.
+func MakeHandler(ctx context.Context, svc ws.Service, l *slog.Logger, instanceID string) http.Handler {
+	logger = l
+
+	mux := chi.NewRouter()
+	mux.Get("/ch/{chanID}/msg", handshake(ctx, svc))
+	mux.Get("/ch/{chanID}/msg/*", handshake(ctx, svc))
+
+	mux.Get("/health", mitras.Health(service, instanceID))
+	mux.Handle("/metrics", promhttp.Handler())
+
+	return mux
+}
