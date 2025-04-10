5a33cf6d689d182abe3875937d2b0bcf6b36d25b
hantdev <captainnemot1k60@gmail.com>
2025-04-10 22:30:08 +0700

    refactor: Remove unused TokenType and related logic from authentication package

--- pkg/authn/authsvc/authn.go
+++ pkg/authn/authsvc/authn.go
@@ -2,17 +2,14 @@ package authsvc
 
 import (
 	"context"
-	"strings"
 
-	grpcAuthV1 "github.com/hantdev/mitras/api/grpc/auth/v1"
 	"github.com/hantdev/mitras/auth/api/grpc/auth"
+	grpcAuthV1 "github.com/hantdev/mitras/internal/grpc/auth/v1"
 	"github.com/hantdev/mitras/pkg/authn"
 	"github.com/hantdev/mitras/pkg/errors"
 	"github.com/hantdev/mitras/pkg/grpcclient"
 	grpchealth "google.golang.org/grpc/health/grpc_health_v1"
 )
-
-const patPrefix = "pat_"
 
 type authentication struct {
 	authSvcClient grpcAuthV1.AuthServiceClient
@@ -38,18 +35,9 @@ func NewAuthentication(ctx context.Context, cfg grpcclient.Config) (authn.Authen
 }
 
 func (a authentication) Authenticate(ctx context.Context, token string) (authn.Session, error) {
-	if strings.HasPrefix(token, patPrefix) {
-		res, err := a.authSvcClient.AuthenticatePAT(ctx, &grpcAuthV1.AuthNReq{Token: token})
-		if err != nil {
-			return authn.Session{}, errors.Wrap(errors.ErrAuthentication, err)
-		}
-
-		return authn.Session{Type: authn.PersonalAccessToken, PatID: res.GetId(), UserID: res.GetUserId()}, nil
-	}
 	res, err := a.authSvcClient.Authenticate(ctx, &grpcAuthV1.AuthNReq{Token: token})
 	if err != nil {
 		return authn.Session{}, errors.Wrap(errors.ErrAuthentication, err)
 	}
-
-	return authn.Session{Type: authn.AccessToken, DomainUserID: res.GetId(), UserID: res.GetUserId(), DomainID: res.GetDomainId()}, nil
+	return authn.Session{DomainUserID: res.GetId(), UserID: res.GetUserId(), DomainID: res.GetDomainId()}, nil
 }
