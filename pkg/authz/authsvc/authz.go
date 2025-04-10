c0da8626db61585ecc96b973e397b7329745a785
hantdev <captainnemot1k60@gmail.com>
2025-04-10 22:31:45 +0700

    refactor: Simplify authorization structure by removing unused domains logic and related mock

--- pkg/authz/authsvc/authz.go
+++ pkg/authz/authsvc/authz.go
@@ -3,26 +3,21 @@ package authsvc
 import (
 	"context"
 
-	grpcAuthV1 "github.com/hantdev/mitras/api/grpc/auth/v1"
 	"github.com/hantdev/mitras/auth/api/grpc/auth"
-	"github.com/hantdev/mitras/domains"
+	grpcAuthV1 "github.com/hantdev/mitras/internal/grpc/auth/v1"
 	"github.com/hantdev/mitras/pkg/authz"
-	pkgDomians "github.com/hantdev/mitras/pkg/domains"
 	"github.com/hantdev/mitras/pkg/errors"
-	svcerr "github.com/hantdev/mitras/pkg/errors/service"
 	"github.com/hantdev/mitras/pkg/grpcclient"
-	"github.com/hantdev/mitras/pkg/policies"
 	grpchealth "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type authorization struct {
 	authSvcClient grpcAuthV1.AuthServiceClient
-	domains       pkgDomians.Authorization
 }
 
 var _ authz.Authorization = (*authorization)(nil)
 
-func NewAuthorization(ctx context.Context, cfg grpcclient.Config, domainsAuthz pkgDomians.Authorization) (authz.Authorization, grpcclient.Handler, error) {
+func NewAuthorization(ctx context.Context, cfg grpcclient.Config) (authz.Authorization, grpcclient.Handler, error) {
 	client, err := grpcclient.NewHandler(cfg)
 	if err != nil {
 		return nil, nil, err
@@ -36,26 +31,10 @@ func NewAuthorization(ctx context.Context, cfg grpcclient.Config, domainsAuthz p
 		return nil, nil, grpcclient.ErrSvcNotServing
 	}
 	authSvcClient := auth.NewAuthClient(client.Connection(), cfg.Timeout)
-	return authorization{
-		authSvcClient: authSvcClient,
-		domains:       domainsAuthz,
-	}, client, nil
+	return authorization{authSvcClient}, client, nil
 }
 
 func (a authorization) Authorize(ctx context.Context, pr authz.PolicyReq) error {
-	if pr.SubjectType == policies.UserType && (pr.ObjectType == policies.GroupType || pr.ObjectType == policies.ClientType || pr.ObjectType == policies.DomainType) {
-		domainID := pr.Domain
-		if domainID == "" {
-			if pr.ObjectType != policies.DomainType {
-				return svcerr.ErrDomainAuthorization
-			}
-			domainID = pr.Object
-		}
-		if err := a.checkDomain(ctx, pr.SubjectType, pr.Subject, domainID); err != nil {
-			return errors.Wrap(svcerr.ErrDomainAuthorization, err)
-		}
-	}
-
 	req := grpcAuthV1.AuthZReq{
 		Domain:          pr.Domain,
 		SubjectType:     pr.SubjectType,
@@ -72,67 +51,6 @@ func (a authorization) Authorize(ctx context.Context, pr authz.PolicyReq) error 
 		return errors.Wrap(errors.ErrAuthorization, err)
 	}
 	if !res.GetAuthorized() {
-		return errors.ErrAuthorization
-	}
-	return nil
-}
-
-func (a authorization) checkDomain(ctx context.Context, subjectType, subject, domainID string) error {
-	dom, err := a.domains.RetrieveEntity(ctx, domainID)
-	if err != nil {
-		return errors.Wrap(svcerr.ErrViewEntity, err)
-	}
-
-	switch dom.Status {
-	case domains.FreezeStatus:
-		_, err := a.authSvcClient.Authorize(ctx, &grpcAuthV1.AuthZReq{
-			Subject:     subject,
-			SubjectType: subjectType,
-			Permission:  policies.AdminPermission,
-			Object:      policies.MitrasObject,
-			ObjectType:  policies.PlatformType,
-		})
-
-		return err
-	case domains.DisabledStatus:
-		_, err := a.authSvcClient.Authorize(ctx, &grpcAuthV1.AuthZReq{
-			Subject:     subject,
-			SubjectType: subjectType,
-			Permission:  policies.AdminPermission,
-			Object:      domainID,
-			ObjectType:  policies.DomainType,
-		})
-
-		return err
-	case domains.EnabledStatus:
-		_, err := a.authSvcClient.Authorize(ctx, &grpcAuthV1.AuthZReq{
-			Subject:     subject,
-			SubjectType: subjectType,
-			Permission:  policies.MembershipPermission,
-			Object:      domainID,
-			ObjectType:  policies.DomainType,
-		})
-
-		return err
-	default:
-		return svcerr.ErrInvalidStatus
-	}
-}
-
-func (a authorization) AuthorizePAT(ctx context.Context, pr authz.PatReq) error {
-	req := grpcAuthV1.AuthZPatReq{
-		UserId:           pr.UserID,
-		PatId:            pr.PatID,
-		EntityType:       uint32(pr.EntityType),
-		OptionalDomainId: pr.OptionalDomainID,
-		Operation:        uint32(pr.Operation),
-		EntityId:         pr.EntityID,
-	}
-	res, err := a.authSvcClient.AuthorizePAT(ctx, &req)
-	if err != nil {
-		return errors.Wrap(errors.ErrAuthorization, err)
-	}
-	if !res.Authorized {
 		return errors.ErrAuthorization
 	}
 	return nil
